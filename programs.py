d80765d77e2b3cf988c57c2c5ec559638e9d3855
amar.saraswat <amar.saraswat@senecaglobal.com>
2024-04-15 09:04:39 +0530

    programs are added.

--- programs.py
+++ programs.py
@@ -1,4 +1,4 @@
-#Find the Factorial of a Number.
+# Find the Factorial of a Number.
 
 def factorial(n):
     if n < 0:
@@ -7,11 +7,52 @@ def factorial(n):
         return 1
     else:
         fact = 1
-        while(n > 1):
+        while n > 1:
             fact *= n
             n -= 1
         return fact
 
+
 num = 5
-print("Factorial of",num,"is",
-factorial(num))
+print("Factorial of", num, "is",
+      factorial(num))
+
+
+# Python program to find the maximum of two numbers
+def maximum_of_two_numbers(num1, num2):
+    if num1 >= num2:
+        return num1
+    else:
+        return num2
+
+
+# Driver code
+num1 = 2
+num2 = 4
+print(maximum_of_two_numbers(num1, num2))
+
+
+# Python3 program to swap first and last element of a list
+
+# Swap function
+def swap_list_elements(new_list):
+    new_list[0], new_list[-1] = new_list[-1], new_list[0]
+    return new_list
+
+
+# Driver code
+new_list = [12, 35, 9, 56, 24]
+print(swap_list_elements(new_list))
+
+
+# function which return reverse of a string
+def is_palindrome(name):
+    return name == name[::-1]
+
+
+# Driver code
+name = "amar"
+print(is_palindrome(name))
+
+
+
